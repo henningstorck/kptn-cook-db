280792a0fbd610a8d955a8ef76b1c7d9385c4c24
Henning Storck <mail@henningstorck.com>
2025-07-30 17:06:45 +0200

    Reformat project

--- src/test/java/com/henningstorck/kptncookdb/TestKptnCookDbApplication.java
+++ src/test/java/com/henningstorck/kptncookdb/TestKptnCookDbApplication.java
@@ -3,9 +3,7 @@ package com.henningstorck.kptncookdb;
 import org.springframework.boot.SpringApplication;
 
 public class TestKptnCookDbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.from(KptnCookDbApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
-
 }
